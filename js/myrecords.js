5cfb94726f227bf9567454e13ca2c726b23d76e8
shibaoyu <shibaoyu@ymatou.com>
2017-06-06 16:35:45 +0800

    implemented prompt

--- js/myrecords.js
+++ js/myrecords.js
@@ -1,5 +1,39 @@
 $(function(){
-  $('.J_editItem').on('click',function(a,b,c,d){
-    
+  $('.J_editItem').on('click', function(){
+    $.confirm({
+        closeIcon: true,
+        boxWidth: '30%',
+        useBootstrap: false,
+        title: 'Record Edit',
+        content: '' +
+        '<form action="" class="formName">' +
+        '<div class="form-group">' +
+          'From <input type="text" placeholder="HH:MM" class="name form-control" required /> to <input type="text" placeholder="HH:MM" class="name form-control" required /> <br>' +
+          'Subject: <input type="text" class="name form-control" required /> <br>'+
+          'Number of Attendees: <input type="Number" class="name form-control" required /> <br>'+
+        '</div>' +
+        '</form>',
+        buttons: {
+            formSubmit: {
+                text: 'Submit',
+                btnClass: 'btn-blue',
+                action: function () {
+                    location.href = './myrecords.html';
+                }
+            },
+            cancel: function () {
+                //close
+            },
+        },
+        onContentReady: function () {
+            // bind to events
+            var jc = this;
+            this.$content.find('form').on('submit', function (e) {
+                // if the user submits the form by pressing enter in the field.
+                e.preventDefault();
+                jc.$$formSubmit.trigger('click'); // reference the button and click it
+            });
+        }
+    });
   });
-});
+})
